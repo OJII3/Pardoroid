32cdb29212f9b8e0d0e24180dc557b49dea4a43a
ojii3 <84656786+OJII3@users.noreply.github.com>
2025-06-30 11:20:31 +0900

    feat: add cargo

--- flake.nix
+++ flake.nix
@@ -5,17 +5,32 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    inputs@{ self, systems, nixpkgs, flake-parts, ... }:
+    inputs@{ self, systems, nixpkgs, flake-parts, rust-overlay, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = import inputs.systems;
       perSystem = { config, pkgs, system, ... }: {
+        _module.args.pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            rust-overlay.overlays.default
+          ];
+        };
         # When execute `nix develop`, you go in shell installed nil.
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             nodejs
+            pkg-config
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" "rust-analyzer" ];
+              targets = [ "aarch64-unknown-linux-gnu" ];
+            })
           ];
 
           shellHook = ''
